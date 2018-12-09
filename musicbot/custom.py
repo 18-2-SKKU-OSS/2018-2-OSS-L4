5abfe5c870925d096a6c9f62d92cc6d16e82cb41
thtl1999 <thtl2004@hanmail.net>
2018-12-09 23:11:39 +0900

    finished custom.py
    it works fine

--- musicbot/custom.py
+++ musicbot/custom.py
@@ -3,20 +3,21 @@ import os
 import fileinput
 import sys
 
+#file read error handler
 def filecheck(filename):
     if filename == 'custom_command.txt':
-        filepath = 'config/'
+        filepath = '../config/'
     else:
-        filepath = 'musicbot/'
+        filepath = ''
 
     if not os.path.isfile(filepath + filename):
         print('Cannot find file',filename)
         exit(1)
 
-#configparser cannot read international characters
+#configparser cannot read international characters. code write required
 def readcommand(config_file):
     filecheck('custom_command.txt')
-    f_commands = open('config/custom_command.txt',encoding='UTF8')
+    f_commands = open('../config/custom_command.txt',encoding='UTF8')
     lines = f_commands.readlines()
 
     commands = {}
@@ -26,18 +27,36 @@ def readcommand(config_file):
             #delete space, 'feff' exception, delete '\n'
             commands[item[0].replace(' ','').replace('\ufeff','')] = 'cmd_' + item[1].replace(' ','').replace('\n','')
     f_commands.close()
-
     return commands
 
+#erase contents and write string in r+ file
+def clearwrite(file,string):
+    file.seek(0)
+    file.truncate(0)
+    file.write(string)
 
+#main
+#check if makred
+filecheck('bot.py')
+f_botpy = open('bot.py','r+',encoding='UTF8')
 
-#check if back-up exist
-if not os.path.isfile('musicbot/bot.back'):
-    #if back-up is not exist
-    f1 = open('musicbot/bot.back','w')
-    f2 = open('musicbot/bot.py','r')
-    f1.write(f2.read())
-    f1.close()
-    f2.close()
+#if modified, restore back-up
+if "#custom command" in f_botpy.readline():
+    filecheck('bot.back')
+    f_backup = open('bot.back', 'r', encoding='UTF8')
+    clearwrite(f_botpy,f_backup.read())
+# if not modified, make back-up
+else:
+    f_backup = open('bot.back', 'w', encoding='UTF8')
+    f_backup.write(f_botpy.read())
+f_botpy.close()
+f_backup.close()
 
-
+#modify and mark bot.py
+f_botpy = open('bot.py','r+',encoding='UTF8')
+strings = f_botpy.read()
+commands = readcommand('../config/custom_command.txt')
+for key, value in commands.items():
+    strings = strings.replace(key,value)
+clearwrite(f_botpy,'#custom command\n' + strings)
+f_botpy.close()
